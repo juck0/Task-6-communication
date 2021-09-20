8d4b7aa8cd6859fd22f497e7c8abb7179a593a5f
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-09-20 07:23:41 +0300

    remove observable

--- app/src/main/java/com/example/task6/MainActivity.kt
+++ app/src/main/java/com/example/task6/MainActivity.kt
@@ -11,8 +11,6 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    lateinit var observable: CompositeDisposable
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
