f3a5e31895b58162dd366e2e8b89d4ee2eb24bd9
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-09-22 10:56:38 +0300

    converting cold to hot

--- app/src/main/java/com/example/task6/MainActivity.kt
+++ app/src/main/java/com/example/task6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.task6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.widget.doOnTextChanged
-
 import com.example.task6.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.disposables.CompositeDisposable
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,13 +21,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun setData() {
         val observable = Observable.create<String> { emitter ->
-            binding.inputText.doOnTextChanged { text, start, before, count ->
+            binding.inputText.doOnTextChanged { text,_,_,_ ->
                 emitter.onNext(text.toString())
             }
-        }
-        observable.subscribe() { it ->
+        }.debounce(1500,TimeUnit.MILLISECONDS).publish()
+        observable.connect()
+        observable.subscribe({ it ->
             binding.textDown.text = it.toString()
-        }
+        },
+            { it ->
+                Log.i("Aymn", it.message.toString())
+
+            })
 
 
     }
