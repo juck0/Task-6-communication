3ae230059614b9d81fde9e74776cdb4d5c953285
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-09-22 10:59:58 +0300

    converting cold observable to hot observable(yachowy)

--- app/src/main/java/com/example/task6/MainActivity.kt
+++ app/src/main/java/com/example/task6/MainActivity.kt
@@ -2,11 +2,9 @@ package com.example.task6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.core.widget.doOnTextChanged
 import com.example.task6.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.core.Observable
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,20 +19,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun setData() {
         val observable = Observable.create<String> { emitter ->
-            binding.inputText.doOnTextChanged { text,_,_,_ ->
+            binding.inputText.doOnTextChanged { text,_, _, _ ->
                 emitter.onNext(text.toString())
             }
-        }.debounce(1500,TimeUnit.MILLISECONDS).publish()
-        observable.connect()
-        observable.subscribe({ it ->
+        }
+        observable.subscribe() { it ->
             binding.textDown.text = it.toString()
-        },
-            { it ->
-                Log.i("Aymn", it.message.toString())
-
-            })
-
-
+        }
     }
 
 
