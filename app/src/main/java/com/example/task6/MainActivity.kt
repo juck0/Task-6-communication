bd3d2e4f07363a0aded5f87e4ad68254e9d36134
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-09-24 12:52:11 +0300

    add function setData to UpFragment

--- app/src/main/java/com/example/task6/MainActivity.kt
+++ app/src/main/java/com/example/task6/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.task6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.widget.doOnTextChanged
 import com.example.task6.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.core.Observable
@@ -9,14 +10,14 @@ import io.reactivex.rxjava3.core.Observable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    val upFragment = UpFragment()
+     val upFragment = UpFragment()
     val downFragment = DownFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setData()
+        upFragment.setData()
         trans()
     }
 
@@ -27,14 +28,5 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-//    private fun setData() {
-//        val observable = Observable.create<String> { emitter ->
-//            binding.inputText.doOnTextChanged { text,_, _, _ ->
-//                emitter.onNext(text.toString())
-//            }
-//        }
-//        observable.subscribe() { it ->
-//            binding.textDown.text = it.toString()
-//        }
-//    }
+
 }
--- app/src/main/java/com/example/task6/UpFragment.kt
+++ app/src/main/java/com/example/task6/UpFragment.kt
@@ -1,13 +1,17 @@
 package com.example.task6
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import com.example.task6.databinding.FragmentUpBinding
+import io.reactivex.rxjava3.core.Observable
 
 class UpFragment : Fragment() {
-
+    private lateinit var binding: FragmentUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +19,37 @@ class UpFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_up, container, false)
-    }}
+    }
+
+     fun setData() {
+        val observable = Observable.create<String> { emitter ->
+            binding.inputText.doOnTextChanged { text,_, _, _ ->
+                emitter.onNext(text.toString())
+            }
+        }
+        observable.subscribe({ it ->
+            binding.textDown.text = it.toString()
+        },
+            {
+                Log.i("onError", it.message.toString())
+
+
+            }
+
+
+        )
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
