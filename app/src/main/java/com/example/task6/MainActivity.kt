b2e9ebd34381e85ded0fa0ec2fbdc1c80558e74d
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-09-24 10:36:40 +0300

    refactor ui and create classes

--- app/src/main/java/com/example/task6/MainActivity.kt
+++ app/src/main/java/com/example/task6/MainActivity.kt
@@ -9,24 +9,32 @@ import io.reactivex.rxjava3.core.Observable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val upFragment = UpFragment()
+    val downFragment = DownFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setData()
+        //setData()
+        trans()
+    }
+
+    private fun trans() {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_up,upFragment)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_down,downFragment)
+
     }
 
 
-    private fun setData() {
-        val observable = Observable.create<String> { emitter ->
-            binding.inputText.doOnTextChanged { text,_, _, _ ->
-                emitter.onNext(text.toString())
-            }
-        }
-        observable.subscribe() { it ->
-            binding.textDown.text = it.toString()
-        }
-    }
-
-
+//    private fun setData() {
+//        val observable = Observable.create<String> { emitter ->
+//            binding.inputText.doOnTextChanged { text,_, _, _ ->
+//                emitter.onNext(text.toString())
+//            }
+//        }
+//        observable.subscribe() { it ->
+//            binding.textDown.text = it.toString()
+//        }
+//    }
 }
